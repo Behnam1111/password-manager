27c9057b03910aa611c6d983cee9e585c67be131
Behnam Bahrami <behnambahrami31@gmail.com>
2021-08-31 15:42:57 +0430

    Save password into a file.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from tkinter import *
+
 WHITE = '#FFFFFF'
 FONT_NAME = "Hourier"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open('data.txt', 'a') as data:
+        record = f"{website} | {email} | {password}\n"
+        data.write(record)
+
+    website_entry.delete(0, 'end')
+    email_entry.delete(0, 'end')
+    password_entry.delete(0, 'end')
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -16,12 +32,14 @@ website_label.grid(row=1, column=0)
 
 website_entry = Entry(width=37)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 
 email_label = Label(text='Email/Username:', bg=WHITE)
 email_label.grid(row=2, column=0)
 
 email_entry = Entry(width=37)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, 'Behnam@gmail.com')
 
 password_label = Label(text="Password:", bg=WHITE)
 password_label.grid(row=3, column=0)
@@ -32,7 +50,7 @@ password_entry.grid(row=3, column=1)
 password_generator_button = Button(text="Generate Password", bg=WHITE, font=(FONT_NAME, 5, 'bold'), width=15)
 password_generator_button.grid(row=3, column=2)
 
-add_button = Button(text="Add Password", bg=WHITE, width=46, font=(FONT_NAME, 6, 'bold'))
+add_button = Button(text="Add Password", bg=WHITE, width=46, font=(FONT_NAME, 6, 'bold'), command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 canvas = Canvas(width=200, height=200, bg=WHITE, highlightthickness=0)
