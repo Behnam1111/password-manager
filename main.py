96c07a4da11d87e1530ebb5f172f772b4fd731f8
Behnam Bahrami <behnambahrami31@gmail.com>
2021-08-31 14:28:36 +0430

    Adding entries and labels

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from tkinter import *
-
+WHITE = '#FFFFFF'
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50, bg=WHITE)
 
-canvas = Canvas(width=200, height=200)
+website_label = Label(text='Website:', bg=WHITE)
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=37)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_label = Label(text='Email/Username:', bg=WHITE)
+email_label.grid(row=2, column=0)
+
+email_entry = Entry(width=37)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:", bg=WHITE)
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=18)
+password_entry.grid(row=3, column=1)
+
+password_generator_button = Button(text="Generate Password", bg=WHITE)
+password_generator_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", bg=WHITE, width=35)
+add_button.grid(row=4, column=1, columnspan=2)
+
+canvas = Canvas(width=200, height=200, bg=WHITE, highlightthickness=0)
 password_image = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=password_image)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
 window.mainloop()
